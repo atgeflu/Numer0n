e3806c7d47b403c3f8f5f32c91eb89457f05cc2b
atgeflu <atgeflu@gmail.com>
2016-07-03 17:52:18 +0200

    Added array shuffle function based on Fisher-Yates shuffle algorithm

---
+++ src/js/gameLogic.js
@@ -0,0 +1,24 @@
+
+
+/**
+  * @desc shuffles an array using the Fisher-Yates shuffle algorithm
+  * @param {array} array - The array to be shuffled
+  * @return {array} array - Returns the shuffled array, even though this is not necessary, since it's shuffled in place
+*/
+function shuffle(array) {
+	var l = array.length;
+    var temp;
+    var i;
+
+    // While there remain elements to shuffle…
+    while(l) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * l--);
+        // And swap it with the current element.
+        temp = array[l];
+        array[l] = array[i];
+        array[i] = temp;
+    }
+
+    return array;
+}
