c28c6c6d42d79d283e8c5c201dc2f1b0a6565b77
atgeflu <atgeflu@gmail.com>
2016-07-04 23:04:21 +0200

    Added high-low functions

--- src/js/gameLogic.js
+++ src/js/gameLogic.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 /**
   * @desc shuffles an array using the Fisher-Yates shuffle algorithm
@@ -21,4 +21,43 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+
+/*
+ * ITEMS
+ */
+
+/**
+  * @desc given a number (between 0 and 9) returns whether the number is HIGH or LOW, where HIGH denotes numbers 5-9 and LOW denotes numbers 0-4
+  * @param {number} intInput - The input number (should be between 0 and 9)
+  * @return {string} strOutput - Returns an string of type HIGH, LOW
+*/ 
+
+function highLow(number) {
+    if(Number.isInteger(number)) {
+        if(number >= 0 && number <= 4) {
+            return "LOW";
+        } else if (number >= 5 && number <= 9) {
+            return "HIGH";
+        } else {
+            throw new Error("integer not in range 0-9");
+        }
+    } else {
+        throw new Error("not an integer");
+    }
+}
+
+/**
+  * @desc given an array of numbers between 0 and 9 return an array of entries of type HIGH, LOW where HIGH denotes numbers 5-9 and LOW denotes numbers 0-4
+  * @param {array} arrInput - The input array containing numbers selected from the range 0-9
+  * @return {array} arrOutput - Returns an array of same length as input array with entries of the type HIGH, LOW
+*/
+
+function highLowArray(arrInput) {
+    if(Array.isArray(arrInput)) {
+        return arrInput.map(highLow);
+    } else {
+        throw new Error("not an array");
+    }
 }
